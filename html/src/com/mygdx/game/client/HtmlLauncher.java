603358318c00cd3c8e96f1cd35555e2c151cf517
miguelwisneski <85126042+miguelwisneski@users.noreply.github.com>
2022-08-10 13:26:03 -0300

    Add files via upload

---
+++ html/src/com/mygdx/game/client/HtmlLauncher.java
@@ -0,0 +1,23 @@
+package com.mygdx.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.mygdx.game.Start;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                // Resizable application, uses available space in browser
+                return new GwtApplicationConfiguration(800, 600);
+                // Fixed size application:
+                //return new GwtApplicationConfiguration(480, 320);
+                
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Start();
+        }
+}
